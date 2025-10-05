d26f33ece00851e6ab87bced2666b2fc83d7e571
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2025-10-05 18:59:48 +0530

    Added glob path for --pdf option in embed_pdf_to_vector_store.py

--- embed_pdf_to_vector_store.py
+++ embed_pdf_to_vector_store.py
@@ -1,6 +1,7 @@
 import argparse
 import hashlib
 import uuid
+from glob import glob
 
 from docling.chunking import HybridChunker
 from langchain_core.documents import Document
@@ -28,28 +29,34 @@ def main() -> None:
         required=True,
         help='Config yaml file to use. See conf/application folder for examples',
     )
-    parser.add_argument('--pdf', required=True, help='Path of the pdf file to process')
+    parser.add_argument(
+        '--pdf',
+        required=True,
+        help='Path of the pdf file to process. Note that glob path is also supported.',
+    )
     args = parser.parse_args()
     app_config = AppConfig.from_yaml(args.conf)
     print(f'{app_config=}')
     converter = app_config.docling_config.docling_pdf_converter()
-    file_path = args.pdf
-    document = converter.convert(file_path).document
+    file_paths = glob(args.pdf)
     chunker = HybridChunker()
     meta_extractor = MetaExtractor()
-    chunks = (
-        Document(
-            page_content=chunker.contextualize(chunk=chunk),
-            metadata=meta_extractor.extract_chunk_meta(
-                file_path=file_path,
-                chunk=chunk,
-            ),
-            id=make_doc_id(f'{file_path}-chunk-{chunk_id}'),
-        )
-        for chunk_id, chunk in enumerate(chunker.chunk(document), 1)
-    )
     embeddings = app_config.embedding_config.langchain_embedding()
-    _ = app_config.vector_store_config.from_documents(chunks, embeddings)
+    for file_path in file_paths:
+        print(f'Embedding file: {file_path}')
+        document = converter.convert(file_path).document
+        chunks = (
+            Document(
+                page_content=chunker.contextualize(chunk=chunk),
+                metadata=meta_extractor.extract_chunk_meta(
+                    file_path=file_path,
+                    chunk=chunk,
+                ),
+                id=make_doc_id(f'{file_path}-chunk-{chunk_id}'),
+            )
+            for chunk_id, chunk in enumerate(chunker.chunk(document), 1)
+        )
+        _ = app_config.vector_store_config.from_documents(chunks, embeddings)
 
 
 if __name__ == '__main__':
