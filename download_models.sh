51162a3d76e622bb83397c791f8d4b8d65da43e4
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2025-10-11 02:25:33 +0530

    Downloading only selected docling models

--- download_models.sh
+++ download_models.sh
@@ -11,6 +11,16 @@ if [[ "$DOCLING_ARTIFACTS_PATH" == "" ]]; then
     exit 1
 fi
 
-# Assuming DOCLING_ARTIFACTS_PATH is present
+download_hf_repo() {
+    local model_repo=$1
+    "$SCRIPT_DIR"/.venv/bin/docling-tools models download-hf-repo "$model_repo" -o "$DOCLING_ARTIFACTS_PATH"/
+}
 
-"$SCRIPT_DIR"/.venv/bin/docling-tools models download -o $DOCLING_ARTIFACTS_PATH/
+# Downloading Docling Models
+download_hf_repo   ds4sd/CodeFormulaV2              &
+download_hf_repo   ds4sd/DocumentFigureClassifier   &
+download_hf_repo   ds4sd/docling-layout-heron       &
+download_hf_repo   ds4sd/docling-layout-heron-101   &
+download_hf_repo   ds4sd/docling-models             &
+
+wait
