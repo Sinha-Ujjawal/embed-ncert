b2d1e8b77a7c07613181bb652b527268e08ce150
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2025-10-14 21:06:27 +0530

    Supporting for In-Memory, On-Disk and On-Server Qdrant Vector DBs

--- app_config/vector_store_config.py
+++ app_config/vector_store_config.py
@@ -6,6 +6,7 @@ from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
 from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
 
 
 @dataclass
@@ -20,8 +21,37 @@ class VectorStoreConfig:
 
 
 @dataclass
-class QdrantVectorStoreConfig(VectorStoreConfig):
+class QdrantClientConfig:
+    @abstractmethod
+    def client(self) -> QdrantClient:
+        raise NotImplementedError
+
+
+@dataclass
+class QdrantURLClientConfig(QdrantClientConfig):
     url: str
+
+    def client(self) -> QdrantClient:
+        return QdrantClient(url=self.url)
+
+
+@dataclass
+class QdrantInMemoryClientConfig(QdrantClientConfig):
+    def client(self) -> QdrantClient:
+        return QdrantClient(':memory:')
+
+
+@dataclass
+class QdrantOnDiskClientConfig(QdrantClientConfig):
+    path: str
+
+    def client(self) -> QdrantClient:
+        return QdrantClient(path=self.path)
+
+
+@dataclass
+class QdrantVectorStoreConfig(VectorStoreConfig):
+    client_config: QdrantClientConfig
     collection_name: str
     addnl_conf: dict[str, Any] = field(default_factory=lambda: {})
 
@@ -30,9 +60,9 @@ class QdrantVectorStoreConfig(VectorStoreConfig):
 
     def from_documents(self, docs: Iterable[Document], embeddings: Embeddings) -> VectorStore:
         return QdrantVectorStore.from_documents(
+            client=self.client_config.client(),
             documents=list(docs),
             embedding=embeddings,
-            url=self.url,
             collection_name=self.collection_name,
             **self.addnl_conf,
         )
